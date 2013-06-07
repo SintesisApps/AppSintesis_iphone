f537fe82e6a6a10d8cd57b7fe0fa1012fbadf384
miriam <cansys60@hotmail.com>
2013-06-07 12:21:49 -0500

    index

---
+++ root.js
@@ -0,0 +1,37 @@
+//###############root.js
+// -*- coding: utf-8 -*-
+
+(function ($)
+{ //=================================================================================
+    $(document).ready(function()
+    {
+        $(document).bind("deviceready", init);
+    });
+
+    function init()
+    {
+        var s = 'Device Name: '     + device.name     + '<br />' +
+                            'Device PhoneGap: ' + device.phonegap + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
+
+       // $('#deviceProperties').html(s);
+
+        var platform = device.platform;
+        if(platform.search("iPad") != -1)
+        {
+           // alert("ipad");
+            location.href="index_k.html";
+        }
+
+        if(platform.search("iPhone") != -1)
+        {
+            //alert("iphone");
+            location.href="index_m.html";
+        }
+
+    }
+
+//=================================================================================
+})(jQuery)// JavaScript Document
